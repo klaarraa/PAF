4f1196a53bd3d5860519954949ff5fcde242e6ee
Zrinkaj <78958362+Zrinkaj@users.noreply.github.com>
2025-04-09 00:12:57 +0200

    3 zadatak

---
+++ Vjezbe/Vjezbe_2/zad3.py
@@ -0,0 +1,5 @@
+import kinematika as kin
+print("Prikaz jednolikog gibanja...")
+kin.jednoliko_gibanje(brzina=10, vrijeme=10)
+print("Prikaz kosog hitca...")
+kin.kosi_hitac(brzina=20, kut=45, masa=1.5, vrijeme=5)
