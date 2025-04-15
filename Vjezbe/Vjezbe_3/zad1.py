edeaaf8d5f3b944815bca1e61e00a8621b31036c
Zrinkaj <78958362+Zrinkaj@users.noreply.github.com>
2025-04-15 16:12:46 +0200

    zadatak 1

---
+++ Vjezbe/Vjezbe_3/zad1.py
@@ -0,0 +1,12 @@
+a=5.0
+b=4.935
+print(a-b)
+#Ocekivani rezultat=0.065. Python zaokruzuje broj na najblizu vrijednost jer ne moze tocno predstavit vecinu decimalnih brojeva
+c=0.1
+d=0.2
+e=0.3
+if c+d+e == 0.6:
+    print("Suma je ista")
+else:
+    print("Suma nije ista")
+print(c+d+e)
